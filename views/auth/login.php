45f8f874f08bf87f279e1c633e4a06e3c9105683
Moises Arcos <acm.developer.19@gmail.com>
2022-08-29 22:45:43 -0500

    UpTask-577: 29082022 Creando el formulario de login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,40 @@
-<h1>Login</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form class="formulario" method="POST" action="/">
+            <div class="campo">
+                <label for="email">Email</label>
+                    <input 
+                        type="email"
+                        id="email"
+                        placeholder="Tu email"
+                        name="email"
+                    />
+            </div>
+
+            <div class="campo">
+                <label for="password">Email</label>
+                    <input 
+                        type="password"
+                        id="password"
+                        placeholder="Tu Password"
+                        name="password"
+                    />
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? Obtener una</a>
+            <a href="/olvide">¿Olvidaste tu password?</a>
+        </div>
+
+    </div>  <!--contenedor-sm -->
+
+</div>
