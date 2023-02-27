777660ce573b7296b46da39d16851b448ef55f92
Moises Arcos <acm.developer.19@gmail.com>
2023-02-27 06:06:47 -0600

    UpTask-598: Creando la ruta y controller de Dashboard

---
+++ controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class DashboardController {
+
+    public static function index(Router $router){
+
+        $router->render('dashboard/index', [
+
+        ]);
+
+    }
+
+}
+
+?>
