aceaf171932775f425578533a1111a37e94a03a0
Moises Arcos <acm.developer.19@gmail.com>
2023-03-15 06:59:08 -0600

    UpTask-628: Verificar si el proyecto existe o si pertenece a quien desea añadir una tarea

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
+
 class TareaController {
 
     public static function index() {
@@ -10,7 +12,34 @@ class TareaController {
 
     public static function crear() {
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            echo json_encode($_POST);
+
+            session_start();
+
+            $proyectoId = $_POST['proyectoId'];
+            $proyecto = Proyecto::where('url', $proyectoId);
+
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){
+
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error al agregar la tarea'
+                ];
+
+                echo json_encode($respuesta);
+
+                return;
+
+            }else{
+                $respuesta = [
+                    'tipo' => 'exito',
+                    'mensaje' => 'Tarea agregada correctamente'
+                ];
+
+                echo json_encode($respuesta);
+
+                return;
+            }
+            
         }
     }
 
