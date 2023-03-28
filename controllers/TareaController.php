948cc6fac93348a7d3f748de7a53350aa9f46188
Moises Arcos <acm.developer.19@gmail.com>
2023-03-28 07:43:39 -0600

    UpTask-644: Eliminando Tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -103,7 +103,35 @@ class TareaController {
 
     public static function eliminar() {
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            session_start();
+            
+            // Validar que el proyecto exista
+            $proyecto = Proyecto::where('url', $_POST['proyectoId']);
 
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){
+
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error'
+                ];
+
+                echo json_encode($respuesta);
+
+                return;
+
+            }
+
+            $tarea = new Tarea($_POST);
+            $resultado = $tarea->eliminar();
+
+            
+            $respuesta = [
+                'resultado' => $resultado,
+                'mensaje' => 'Eliminado Correctamente',
+                'tipo' => 'exito'
+            ];
+            echo json_encode($respuesta);
+           
         }
     }
 
