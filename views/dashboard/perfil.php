28941e3d6446ec14ba0525e83e4a29ccc7d69d6b
Moises Arcos <acm.developer.19@gmail.com>
2023-04-01 06:25:00 -0600

    UpTask-658:Creando el formulario de Perfil

--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,3 +1,32 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
+<div class="contenedor-sm">
+    <?php include_once __DIR__ . '/../templates/alertas.php';  ?>
+
+    <form action="" class="formulario" method="POST">
+        <div class="campo">
+            <label for="nombre">Nombre</label>
+            <input
+                type="text"
+                value="<?= $usuario->nombre; ?>"
+                name="nombre"
+                placeholder="Tu Nombre"
+            />
+        </div>
+        <div class="campo">
+            <label for="email">Email</label>
+            <input
+                type="email"
+                value="<?= $usuario->email; ?>"
+                name="email"
+                placeholder="Tu Email"
+            />
+        </div>
+
+        <input type="submit" value="Guardar Cambios" >
+
+    </form>
+
+</div>
+
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
