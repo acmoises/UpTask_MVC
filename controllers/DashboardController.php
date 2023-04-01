dca69a16b4181494114f3d1a81a26d137f6b835b
Moises Arcos <acm.developer.19@gmail.com>
2023-04-01 06:38:05 -0600

    UpTask-659: Validando el Perfil

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -89,6 +89,18 @@ class DashboardController {
         $alertas = [];
 
         $usuario = Usuario::find($_SESSION['id']);
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validar_perfil();
+
+            if(empty($alertas)){
+
+                // Guardar el usuario
+                
+            }
+        }
         
         $router->render('dashboard/perfil', [
             'titulo' => 'Perfil',
