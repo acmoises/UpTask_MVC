28f012cc65ebd8ee8619f5064c9aa70f785df98e
Moises Arcos <acm.developer.19@gmail.com>
2023-04-04 05:55:07 -0600

    UpTask-663: Validando los paswords

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -130,6 +130,17 @@ class DashboardController {
         $alertas = [];
 
         if($_SERVER['REQUEST_METHOD'] === 'POST' ){
+            
+            $usuario = Usuario::find($_SESSION['id']); 
+
+            // Sincronizar con los datos del usuario
+            $usuario->sincronizar($_POST);
+
+            $alertas = $usuario->nuevo_password();
+
+            if(empty($alertas)){
+
+            }
 
         }
 
